771cebfa47a1b87be4f78bcef26e9a0cb3b37487
Nathan Downer <nathandowner123@gmail.com>
2024-02-29 13:31:48 -0500

    update start page for auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@/routes.tsx';
-// import { AuthProvider } from './contexts/AuthContext.tsx';
+import { AuthProvider } from './contexts/AuthContext.tsx';
 import { TitleProvider } from './contexts/TitleContext.tsx';
 import { KeypadProvider } from './contexts/KeypadContext.tsx';
 
 const App = () => {
   return (
-    // <AuthProvider>
-    <TitleProvider>
-      <KeypadProvider>
-        <RouterProvider router={router} />
-      </KeypadProvider>
-    </TitleProvider>
-    // </AuthProvider>
+    <AuthProvider>
+      <TitleProvider>
+        <KeypadProvider>
+          <RouterProvider router={router} />
+        </KeypadProvider>
+      </TitleProvider>
+    </AuthProvider>
   );
 };
 
