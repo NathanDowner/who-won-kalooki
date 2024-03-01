dcdaae039bec63f83a8ee082644a403fb63bad5a
Nathan Downer <32402220+NathanDowner@users.noreply.github.com>
2024-03-01 15:28:57 -0500

    Feature/google auth (#3)
    * add clsx pkg
    
    * update start page for auth
    
    * update add player page with auth

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -8,13 +8,11 @@ import girl1 from '../assets/girl_1.png';
 import girl2 from '../assets/girl_2.png';
 import boy1 from '../assets/boy_1.png';
 import boy2 from '../assets/boy_2.png';
+import { useAuth } from '@/contexts/AuthContext';
+import { clsx } from 'clsx';
 
-// https://i.pravatar.cc/300
-
-type StartPageProps = {};
-
-const StartPage = ({}: StartPageProps) => {
-  // const { user, signInWithGoogle, logout } = useAuth();
+const StartPage = () => {
+  const { user, signInWithGoogle, logout } = useAuth();
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -28,39 +26,45 @@ const StartPage = ({}: StartPageProps) => {
         <h1 className="text-4xl font-bold mb-14">Who Won?</h1>
 
         <div className="grid gap-4 grid-cols-2 grid-rows-2">
-          {/* <img
-            className="w-28 h-28 rounded-full"
-            src={
-              user
-                ? user.photoURL!
-                : 'https://avatar.iran.liara.run/public/girl?username=0'
-            }
-            alt={user ? user.displayName! : 'Random avatar image 1'}
-          /> */}
           <img
             className="w-28 h-28 rounded-full"
-            src={girl1}
-            alt="Random avatar image 1"
+            src={user ? user.photoURL! : girl1}
+            alt={
+              user ? `Image of ${user.displayName}` : 'Random avatar image 1'
+            }
           />
-          <img className="w-28 h-28" src={boy1} alt="Random avatar image 2" />
+          <img
+            className="w-28 h-28 test"
+            src={boy1}
+            alt="Random avatar image 2"
+          />
           <img className="w-28 h-28" src={girl2} alt="Random avatar image 3" />
           <img className="w-28 h-28" src={boy2} alt="Random avatar image 4" />
         </div>
+        {user && <h2 className="text-2xl mt-8">Was it {user.displayName}?</h2>}
       </header>
-      {/* {user ? ( */}
+
+      {/* Buttons */}
       <div className="flex flex-col items-center gap-4">
-        <Link to={AppRoutes.addPlayers} className="btn btn-lg">
-          Start a new game
+        {!user && (
+          <button className="btn btn-lg" onClick={() => signInWithGoogle()}>
+            Login w/ Google
+          </button>
+        )}
+
+        <Link
+          to={AppRoutes.addPlayers}
+          className={clsx('btn btn-lg', { 'btn-link': !user })}
+        >
+          {user ? 'Start a new game' : 'Play as Guest'}
         </Link>
-        {/* <button className="underline" onClick={logout}>
+
+        {user && (
+          <button className="btn btn-link" onClick={logout}>
             Logout
-          </button> */}
+          </button>
+        )}
       </div>
-      {/* ) : (
-        <button className="btn btn-lg" onClick={() => signInWithGoogle()}>
-          Login w/ Google
-        </button>
-      )} */}
     </div>
   );
 };
