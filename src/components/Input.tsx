6d86f8bdc4e62e2c4de496208ae8d4640fddca44
Nathan Downer <nathandowner123@gmail.com>
2024-07-03 18:31:14 -0500

    create input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,55 @@
+import clsx from 'clsx';
+import React from 'react';
+
+interface InputProps {
+  label: string;
+  type?: 'text' | 'email';
+  name?: string;
+  value: string;
+  placeholder?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  hasError?: boolean;
+  smallText?: string;
+  leftIcon?: React.ComponentType<{ className?: string }>;
+  rightIcon?: React.ComponentType<{ className?: string }>;
+  disabled?: boolean;
+}
+
+const Input = ({
+  label,
+  type = 'text',
+  name,
+  value,
+  onChange,
+  placeholder,
+  smallText,
+  hasError = false,
+  disabled = false,
+}: InputProps) => {
+  return (
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">{label}</span>
+      </div>
+      <input
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+        name={name}
+        value={value}
+        className={clsx('input input-bordered w-full max-w-xs', {
+          'input-error': hasError,
+        })}
+        disabled={disabled}
+      />
+      <div className="label -mt-1">
+        <span className={clsx('label-text-alt', { 'text-red-500': hasError })}>
+          {smallText}
+        </span>
+      </div>
+    </label>
+  );
+};
+
+export default Input;
