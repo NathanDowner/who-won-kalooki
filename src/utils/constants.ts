804415e25213821d2460101ad219112f59b73c3e
Nathan Downer <nathandowner123@gmail.com>
2024-08-18 21:34:48 -0500

    add run-tests workflow

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -29,50 +29,50 @@ export const INITIAL_SCORES: Record<string, number[]> = {
 export const PENALTY_AMOUNT = 50;
 export const ANIMATION_DURATION = 0.25;
 
-export const sampleGame = {
-  type: GameType.Kalooki,
-  id: '1WV6PkIkde0ihrMQzmrT',
-  isComplete: false,
-  endedAt: new Timestamp(34344847877, 434),
-  players: [
-    {
-      id: 'r7hnwJKvkBXmwDOIElZll0H7YZy2',
-      name: 'Nathan Downer',
-      imgUrl:
-        'https://lh3.googleusercontent.com/a/ACg8ocIFi7YpIJyNhHDiWZtjh953jvdQPBZsPLmUI9HRXXX7suwe=s96-c',
-    },
-    {
-      imgUrl:
-        'https://avatar.iran.liara.run/public/boy?username=PlayerNatalia Downer',
-      name: 'Natalia Downer',
-    },
-    {
-      name: 'Brittney Chin',
-      imgUrl:
-        'https://avatar.iran.liara.run/public/boy?username=PlayerBrittney Chin',
-    },
-  ],
-  winner: {
-    imgUrl:
-      'https://lh3.googleusercontent.com/a/ACg8ocIFi7YpIJyNhHDiWZtjh953jvdQPBZsPLmUI9HRXXX7suwe=s96-c',
-    name: 'Nathan Downer',
-    id: 'r7hnwJKvkBXmwDOIElZll0H7YZy2',
-  },
-  creator: {
-    id: 'r7hnwJKvkBXmwDOIElZll0H7YZy2',
-    imgUrl:
-      'https://lh3.googleusercontent.com/a/ACg8ocIFi7YpIJyNhHDiWZtjh953jvdQPBZsPLmUI9HRXXX7suwe=s96-c',
-    name: 'Nathan Downer',
-  },
-  scores: {
-    '333': [0, 50, 100],
-    '334': [50, 0, 100],
-    '344': [0, 150, 0],
-    '444': [0, 0, 0],
-    '3333': [0, 0, 0],
-    '3334': [0, 0, 0],
-    '3344': [0, 0, 0],
-    '3444': [0, 0, 0],
-    '4444': [0, 0, 0],
-  },
-};
+// export const sampleGame = {
+//   type: GameType.Kalooki,
+//   id: '1WV6PkIkde0ihrMQzmrT',
+//   isComplete: false,
+//   endedAt: new Timestamp(34344847877, 434),
+//   players: [
+//     {
+//       id: 'r7hnwJKvkBXmwDOIElZll0H7YZy2',
+//       name: 'Nathan Downer',
+//       imgUrl:
+//         'https://lh3.googleusercontent.com/a/ACg8ocIFi7YpIJyNhHDiWZtjh953jvdQPBZsPLmUI9HRXXX7suwe=s96-c',
+//     },
+//     {
+//       imgUrl:
+//         'https://avatar.iran.liara.run/public/boy?username=PlayerNatalia Downer',
+//       name: 'Natalia Downer',
+//     },
+//     {
+//       name: 'Brittney Chin',
+//       imgUrl:
+//         'https://avatar.iran.liara.run/public/boy?username=PlayerBrittney Chin',
+//     },
+//   ],
+//   winner: {
+//     imgUrl:
+//       'https://lh3.googleusercontent.com/a/ACg8ocIFi7YpIJyNhHDiWZtjh953jvdQPBZsPLmUI9HRXXX7suwe=s96-c',
+//     name: 'Nathan Downer',
+//     id: 'r7hnwJKvkBXmwDOIElZll0H7YZy2',
+//   },
+//   creator: {
+//     id: 'r7hnwJKvkBXmwDOIElZll0H7YZy2',
+//     imgUrl:
+//       'https://lh3.googleusercontent.com/a/ACg8ocIFi7YpIJyNhHDiWZtjh953jvdQPBZsPLmUI9HRXXX7suwe=s96-c',
+//     name: 'Nathan Downer',
+//   },
+//   scores: {
+//     '333': [0, 50, 100],
+//     '334': [50, 0, 100],
+//     '344': [0, 150, 0],
+//     '444': [0, 0, 0],
+//     '3333': [0, 0, 0],
+//     '3334': [0, 0, 0],
+//     '3344': [0, 0, 0],
+//     '3444': [0, 0, 0],
+//     '4444': [0, 0, 0],
+//   },
+// };
