6a1d97349313afabf7427764a157423b10c0a7e0
Nathan Downer <nathandowner123@gmail.com>
2024-11-19 18:17:48 -0500

    create reusable firebase hook for making document updates

--- src/hooks/useUpdateDocument.tsx
+++ src/hooks/useUpdateDocument.tsx
@@ -1,33 +1,55 @@
 import { FirebaseError } from 'firebase/app';
-import { Firestore, SetOptions, doc, setDoc } from 'firebase/firestore';
 import { useCallback, useState } from 'react';
+import toast from 'react-hot-toast';
 
-export function useUpdateDocument<TData>(db: Firestore): {
+type UseUpdateDocumentOptions<TResponse> = {
+  onSuccess?: (data: TResponse) => void;
+  successNotificationText?: string;
+  showErrorToast?: boolean;
+};
+
+export function useUpdateDocument<TData, TResponse>(
+  updateFn: (data: TData) => Promise<TResponse>,
+  options: UseUpdateDocumentOptions<TResponse> = { showErrorToast: true },
+): {
   error: FirebaseError | null;
   isLoading: boolean;
-  updateDocument: (path: string, data: Partial<TData>) => Promise<void>;
+  data: TResponse | null;
+  updateDocument: (data: TData) => Promise<void>;
 } {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<FirebaseError | null>(null);
+  const [responseData, setResponseData] = useState<TResponse | null>(null);
 
   const updateDocument = useCallback(
-    async (
-      path: string,
-      data: Partial<TData>,
-      options: SetOptions = { merge: true },
-    ) => {
+    async (data: TData) => {
       setIsLoading(true);
       setError(null);
       try {
-        await setDoc(doc(db, path), data, options);
-      } catch (error: any) {
-        setError(error as FirebaseError);
+        const { onSuccess, successNotificationText } = options;
+        const resp = await updateFn(data);
+        setResponseData(resp);
+
+        if (onSuccess) {
+          onSuccess(resp);
+        }
+        if (successNotificationText) {
+          toast.success(successNotificationText);
+        }
+      } catch (err: unknown) {
+        console.log(err);
+        setError(err as FirebaseError);
+        const { showErrorToast } = options;
+
+        if (showErrorToast) {
+          toast.error(error!.message);
+        }
       } finally {
         setIsLoading(false);
       }
     },
-    [db],
+    [updateFn, options],
   );
 
-  return { isLoading, error, updateDocument };
+  return { isLoading, error, updateDocument, data: responseData };
 }
