9ab2302aa925375bdaefe659dfea6f7ca27a5687
Nathan Downer <nathandowner123@gmail.com>
2024-11-20 20:41:57 -0500

    feat(Button): add customizable styles and sizes to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,46 @@
+import clsx from 'clsx';
 import { PropsWithChildren } from 'react';
 
 interface ButtonProps extends PropsWithChildren {
   type?: 'button' | 'submit';
+  btnStyle?: keyof typeof BUTTON_STYLES;
+  size?: 'sm' | 'md' | 'lg';
   loading?: boolean;
   onClick?: () => void;
   disabled?: boolean;
 }
+
+const BUTTON_STYLES = {
+  primary: 'btn-primary',
+  secondary: 'btn-secondary',
+  outline: 'btn-outline',
+  error: 'btn-error',
+};
+
+const BUTTON_SIZES = {
+  sm: 'btn-sm',
+  md: 'btn-md',
+  lg: 'btn-lg',
+};
 
 const Button = ({
   children,
   loading = false,
   disabled = false,
   onClick,
+  btnStyle = 'primary',
+  size = 'md',
   type = 'button',
 }: ButtonProps) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className="btn btn-primary"
+      className={clsx(
+        'btn border-2 border-black',
+        BUTTON_STYLES[btnStyle],
+        BUTTON_SIZES[size],
+      )}
       disabled={disabled || loading}
     >
       {loading && <span className="loading loading-spinner" />}
