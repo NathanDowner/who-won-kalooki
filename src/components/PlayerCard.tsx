83a9c98b33ef24d6f0a69c1797e0eb365aca465c
Nathan Downer <nathandowner123@gmail.com>
2024-12-03 22:56:11 -0500

    feat(player): enhance PlayerCard with conditional styling and selection indicator

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,6 +1,7 @@
 import Card from '@/components/Card';
 import UserProfileImage from '@/components/UserProfileImage';
 import { SimpleUserProfile } from '@/models/user.model';
+import clsx from 'clsx';
 
 type Props = {
   user: SimpleUserProfile;
@@ -10,13 +11,36 @@ type Props = {
 
 const PlayerCard = ({ user, isSelected, onClick }: Props) => {
   return (
-    <Card onClick={onClick} className="flex gap-4 items-center">
-      <UserProfileImage imgUrl={user.imgUrl} className="w-14 h-14" />
+    <Card
+      onClick={onClick}
+      className={clsx(
+        'flex gap-4 items-center',
+        isSelected && 'border-green-600 bg-green-50',
+      )}
+    >
+      <UserProfileImage
+        imgUrl={user.imgUrl}
+        className={clsx('w-14 h-14', isSelected && 'border-green-600')}
+      />
       <div className="flex flex-col items-start text-xl">
-        <h3 className="truncate-text">{user.fullName}</h3>
+        <h3 className={clsx('truncate-text', isSelected && 'text-green-600')}>
+          {user.fullName}
+        </h3>
 
-        <small className="text-gray-400 italic">@{user.userName}</small>
+        <small
+          className={clsx(
+            'text-gray-400 italic',
+            isSelected && 'text-green-600/60',
+          )}
+        >
+          @{user.userName}
+        </small>
       </div>
+      {isSelected && (
+        <div className="grid place-items-center bg-green-400 rounded-e-sm text-white -m-3 ml-auto self-stretch">
+          <span className="p-2">✔</span>
+        </div>
+      )}
     </Card>
   );
 };
