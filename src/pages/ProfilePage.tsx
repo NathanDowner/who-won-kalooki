6a122025f9256f2018924cfa0f9aa2a795c4caab
Nathan Downer <nathandowner123@gmail.com>
2025-01-17 18:43:07 -0500

    feat(profile): replace game stats display with UserProfileComponent for enhanced user profile view

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,9 +1,7 @@
 import AppHeader from '@/components/AppHeader';
-import Card from '@/components/Card';
-import ProfileHeader from '@/components/ProfileHeader';
 import { useAuth } from '@/contexts/AuthContext';
-import GameStatCard from '@/components/GameStatCard';
-import { GameType } from '@/models/gameType.enum';
+import UserProfileComponent from '@/features/notifications/components/UserProfileComponent';
+import { UserProfile } from '@/models/user.model';
 
 const ProfilePage = () => {
   const { userProfile } = useAuth();
@@ -12,25 +10,10 @@ const ProfilePage = () => {
     <>
       <div className="page">
         <AppHeader title="Profile" />
-        <ProfileHeader userProfile={userProfile!} />
-        <h2 className=" mt-8 mb-4 text-lg font-semibold">Your Games</h2>
-
-        {userProfile?.games ? (
-          Object.keys(userProfile!.games!).map((gameName) => (
-            <GameStatCard
-              key={gameName}
-              gameStats={{
-                gameName,
-                ...userProfile!.games![gameName as GameType],
-              }}
-            />
-          ))
-        ) : (
-          <Card className="text-center flex flex-col">
-            <p className="font-semibold">No games played yet</p>
-            <p>Complete a game to see your stats here!</p>
-          </Card>
-        )}
+        <UserProfileComponent
+          userProfile={userProfile as UserProfile}
+          isCurrentUser
+        />
       </div>
     </>
   );
